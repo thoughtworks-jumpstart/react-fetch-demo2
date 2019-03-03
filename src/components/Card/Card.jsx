ec96814e1407a1e1f531374a0f42e39af71da473
Angeline <atan@thoughtworks.com>
2019-03-04 00:11:26 +0800

    Add margin to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ function Card({ imageUrl, title }) {
   const styles = { width: "18rem" };
 
   return (
-    <div className="card" style={styles}>
+    <div className="card m-3" style={styles}>
       <img src={imageUrl} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
