3eb7085c04ca7e61c10e2eefb20d2f581f480e02
Angeline <atan@thoughtworks.com>
2019-03-03 23:51:07 +0800

    Modify to async await

--- src/service/unsplash.js
+++ src/service/unsplash.js
@@ -1,14 +1,12 @@
 import axios from "axios"
 
-export function getUnsplashAxios(searchTerm){
+export async function getUnsplashAxios(searchTerm){
   const API_KEY = process.env.REACT_APP_API_KEY 
-  console.log(API_KEY)
-  axios.get("https://api.unsplash.com/search/photos", {
+  const response= await axios.get("https://api.unsplash.com/search/photos", {
     params: {query: searchTerm},
     headers: {
       Authorization: `Client-ID ${API_KEY}`
     }
-  }).then((response) => {
-    console.log(response.data.results)
-  })
+  });
+  console.log(response.data.results)
 } 
