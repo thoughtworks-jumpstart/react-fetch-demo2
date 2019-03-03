17a5bea5d28814ea8d94c0c92cfe320f46f632b9
Angeline <atan@thoughtworks.com>
2019-03-03 23:46:34 +0800

    Initial version of app

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card({ imageUrl, title }) {
+  const styles = { width: "18rem" };
+
+  return (
+    <div className="card" style={styles}>
+      <img src={imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
