fb230d48e32b8af8b13ac6908680ab4866d7d689
Angeline <atan@thoughtworks.com>
2019-03-04 15:13:30 +0800

    Pass in headers through fetch options

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ class App extends Component {
     try {
       const searchTerm = "trees";
       const response = await fetch(
-        `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${
-          process.env.REACT_APP_API_KEY
-        }`
+        `https://api.unsplash.com/search/photos?query=${searchTerm}`,
+        {
+          headers: { 
+            Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`
+          }
+        }
       );
       const data = await response.json();
       this.setState({resultList: data.results})
