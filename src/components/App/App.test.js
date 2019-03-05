6cb78933742463141bd4a1749ff27735921b8e0d
Angeline <atan@thoughtworks.com>
2019-03-05 15:21:44 +0800

    changes to App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "react-testing-library/cleanup-after-each";
 import "jest-dom/extend-expect";
-import { render } from "react-testing-library";
+import { render, wait} from "react-testing-library";
 import {searchPhotos as mockSearchPhotos} from "../../services/unsplash";
 import App from "./App";
 
-
 jest.mock("../../services/unsplash", () => {
   const sampleResult = [
-    { id: 1, description: "This pic 1", urls: { small: "http://image" } },
+    { id: 1, description: "This is pic 1", urls: { small: "http://image" } },
     { id: 2, description: "This is pic 2", urls: { small: "http://image" } },
     { id: 3, description: "This is pic 3", urls: { small: "http://image" } }
   ];
@@ -21,12 +20,13 @@ jest.mock("../../services/unsplash", () => {
 });
 
 test("ensure fetch called upon load", async () => {
+
   const { getByText, } = render(<App />);
   expect(mockSearchPhotos).toHaveBeenCalledTimes(1);
   expect(mockSearchPhotos).toHaveBeenCalledWith("flowers");
-  // await wait(() => {
-  //   expect(getByText("This pic 1")).toBeInTheDocument();
-  //   expect(getByText("This pic 2")).toBeInTheDocument();
-  //   expect(getByText("This pic 3")).toBeInTheDocument();
-  // });
-});
+  await wait(() => {
+    expect(getByText("This is pic 1")).toBeInTheDocument();
+    expect(getByText("This is pic 2")).toBeInTheDocument();
+    expect(getByText("This is pic 3")).toBeInTheDocument();
+  });
+}); 
